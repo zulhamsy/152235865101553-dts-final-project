cb4e3e8db08ab2bc95b1da0af74e20a46ee9beaa
Zulham Syafrawi <official.zulham@gmail.com>
2022-07-31 11:53:02 +0700

    header UI

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,34 @@
 import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import blue from "@mui/material/colors/blue";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <AppBar position="static" color="transparent" variant="outlined">
+      <Toolbar>
+        <Typography
+          fontWeight="normal"
+          variant="h6"
+          component="div"
+          sx={{ flexGrow: 1 }}
+        >
+          Game
+          <Typography
+            fontWeight="bold"
+            variant="h6"
+            component="span"
+            color={() => blue[800]}
+          >
+            Blue
+          </Typography>
+        </Typography>
+        <Button color="primary" variant="contained" size="large" sx={{ px: 4 }}>
+          Login
+        </Button>
+      </Toolbar>
+    </AppBar>
+  );
 }
