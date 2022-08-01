2581e76ccf2ca2411c8bff742696d6498057378b
Zulham Syafrawi <official.zulham@gmail.com>
2022-08-01 21:23:14 +0700

    404 page done

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,37 @@
 import React from "react";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
 
 export default function NotFound() {
-  return <div>NotFound</div>;
+  const navigate = useNavigate();
+  return (
+    <Container
+      maxWidth="lg"
+      sx={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Typography
+        variant="h3"
+        textAlign="center"
+        textTransform="uppercase"
+        fontWeight="bold"
+        gutterBottom
+      >
+        Oops! page not found
+      </Typography>
+      <Typography variant="h5" mb={5} color="GrayText">
+        We couldn't find what you're looking for
+      </Typography>
+      <Button onClick={() => navigate("/")} variant="contained" size="large">
+        Go to Home
+      </Button>
+    </Container>
+  );
 }
