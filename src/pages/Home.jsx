41c2386941c2255775a3d0fc72aafd7c3b03beab
Zulham Syafrawi <official.zulham@gmail.com>
2022-08-03 07:48:57 +0700

    use game context for fetching data

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,17 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
 import ListContainer from "../components/ListContainer";
-import data from "../service/mockdata.json";
-import { fetchLiveGames } from "../service/axios";
+import { useGame } from "../context/GameContext";
 
 export default function Home() {
-  useEffect(() => {
-    fetchLiveGames().then((response) => {
-      console.log(response);
-    });
-  });
+  const gameList = useGame();
   return (
     <Container sx={{ mt: 2 }}>
       <Typography variant="h4" component="h1" color="grey.600" mb={3}>
         Live Games
       </Typography>
-      <ListContainer list={data} />
+      <ListContainer list={gameList.slice(0, 10)} />
     </Container>
   );
 }
