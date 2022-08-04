52672e15a31d7ef1ee658d9b4d2aa49a4158fe3a
Zulham Syafrawi <official.zulham@gmail.com>
2022-08-04 11:35:08 +0700

    detail page worked

--- src/pages/GameDetail.jsx
+++ src/pages/GameDetail.jsx
@@ -1,5 +1,137 @@
-import React from "react";
+import {
+  Container,
+  Grid,
+  Stack,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchGameWithId } from "../service/axios";
 
 export default function GameDetail() {
-  return <div>GameDetail</div>;
+  const [gameData, setGameData] = useState({});
+  const { gameId } = useParams();
+
+  useEffect(
+    function () {
+      async function fetchGame() {
+        const response = await fetchGameWithId(gameId);
+        const data = response.data;
+        setGameData(data);
+      }
+
+      fetchGame();
+    },
+    [gameId],
+  );
+
+  return (
+    <Container sx={{ mt: 2 }}>
+      <Grid container>
+        {/* Image */}
+        <Grid item sm={4}>
+          <img src={gameData.thumbnail} alt={gameData.title} />
+          <List>
+            <ListItem>
+              <ListItemText
+                primary="Developer"
+                secondary={gameData.developer}
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                primary="Publisher"
+                secondary={gameData.publisher}
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText
+                primary="Release Date"
+                secondary={gameData.release_date}
+              />
+            </ListItem>
+            <ListItem>
+              <ListItemText primary="Platform" secondary={gameData.platform} />
+            </ListItem>
+          </List>
+        </Grid>
+        {/* Content */}
+        <Grid item sm={8}>
+          <Stack spacing={4}>
+            {/* Description */}
+            <div>
+              <Typography
+                variant="h5"
+                component="h2"
+                fontWeight={500}
+                color="darkblue"
+                mb={2}
+              >
+                About {gameData.title}
+              </Typography>
+              <Typography variant="body1" whiteSpace="pre-line">
+                {gameData.description}
+              </Typography>
+            </div>
+            {/* Minimum Specs */}
+            {gameData.minimum_system_requirements && (
+              <div>
+                <Typography
+                  variant="h5"
+                  component="h2"
+                  fontWeight={500}
+                  color="darkblue"
+                >
+                  Minimum System Requirement
+                </Typography>
+                <Grid container>
+                  <Grid item sm={5}>
+                    <List>
+                      <ListItem>
+                        <ListItemText
+                          primary="Operating System"
+                          secondary={gameData.minimum_system_requirements.os}
+                        />
+                      </ListItem>
+                      <ListItem>
+                        <ListItemText
+                          primary="Memory"
+                          secondary={
+                            gameData.minimum_system_requirements.memory
+                          }
+                        />
+                      </ListItem>
+                    </List>
+                  </Grid>
+                  <Grid item sm={5}>
+                    <List>
+                      <ListItem>
+                        <ListItemText
+                          primary="Processor"
+                          secondary={
+                            gameData.minimum_system_requirements.processor
+                          }
+                        />
+                      </ListItem>
+                      <ListItem>
+                        <ListItemText
+                          primary="Graphics"
+                          secondary={
+                            gameData.minimum_system_requirements.graphics
+                          }
+                        />
+                      </ListItem>
+                    </List>
+                  </Grid>
+                </Grid>
+              </div>
+            )}
+          </Stack>
+        </Grid>
+      </Grid>
+    </Container>
+  );
 }
--- src/service/axios.js
+++ src/service/axios.js
@@ -3,16 +3,25 @@ import axios from "axios";
 const BASE_URL = "https://free-to-play-games-database.p.rapidapi.com/api/games";
 const API_HOST = "free-to-play-games-database.p.rapidapi.com";
 const API_KEY = "3214b07db7msh5107f3820563dc3p140b02jsn314d76de900b";
+const options = {
+  method: "GET",
+  url: BASE_URL,
+  headers: {
+    "X-RapidAPI-Key": API_KEY,
+    "X-RapidAPI-Host": API_HOST,
+  },
+};
 
 export async function fetchLiveGames() {
-  const options = {
-    method: "GET",
-    url: BASE_URL,
-    headers: {
-      "X-RapidAPI-Key": API_KEY,
-      "X-RapidAPI-Host": API_HOST,
-    },
+  return axios.request(options);
+}
+
+export async function fetchGameWithId(id) {
+  const config = {
+    ...options,
+    url: "https://free-to-play-games-database.p.rapidapi.com/api/game",
+    params: { id: id },
   };
 
-  return axios.request(options);
+  return axios.request(config);
 }
